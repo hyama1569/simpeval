b83cb04c2b6d3812865e602ce63095faebdd50ac
hyama1569 <81945930+hyama1569@users.noreply.github.com>
2022-06-13 20:40:11 +0900

    Update setup.sh

--- finetune/wikiauto/wikiauto_src/exp_1/setup.sh
+++ finetune/wikiauto/wikiauto_src/exp_1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -P ../data https://www.dropbox.com/sh/ohqaw41v48c7e5p/AAATBDhU1zpdcT5x5WgO8DMaa/wiki-auto-all-data/wiki-auto-part-1-data.json?dl=0
+wget -P ../../data https://www.dropbox.com/sh/ohqaw41v48c7e5p/AAATBDhU1zpdcT5x5WgO8DMaa/wiki-auto-all-data/wiki-auto-part-1-data.json?dl=0
 
 pip install transformers
 pip install git+https://github.com/PyTorchLightning/pytorch-lightning
@@ -8,4 +8,4 @@ pip install hydra-core --upgrade
 pip install wandb
 wandb login
 
-python ../data/prepare_data.py
+python ../../data/prepare_data.py
