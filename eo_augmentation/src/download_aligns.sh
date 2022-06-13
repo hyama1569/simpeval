effcee4eb74a5c626662d350f96afa8a894aa73a
hyama1569 <81945930+hyama1569@users.noreply.github.com>
2022-06-14 01:45:31 +0900

    Delete download_aligns.sh

--- eo_augmentation/src/download_aligns.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1JS7UeElzTGr0eQZ-H_556CiQif15LuEa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JS7UeElzTGr0eQZ-H_556CiQif15LuEa" -O aligns_wikiauto.pickle && rm -rf /tmp/cookies.txt
