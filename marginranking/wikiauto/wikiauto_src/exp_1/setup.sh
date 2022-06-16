24b599f430707720214848a03b5d34945004a557
Hikaru Yamanaka <sqrt1569@gmail.com>
2022-06-17 00:13:50 +0900

    revised

--- marginranking/wikiauto/wikiauto_src/exp_1/setup.sh
+++ marginranking/wikiauto/wikiauto_src/exp_1/setup.sh
@@ -18,10 +18,17 @@ wget -P /workspace/simpeval/marginranking/wikiauto/data/ --load-cookies /tmp/coo
 #aug_data
 wget -P /workspace/simpeval/marginranking/wikiauto/data/ --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1vxWDSs4Y52l-EUoojRwvIBpbvLuWJjfc' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1vxWDSs4Y52l-EUoojRwvIBpbvLuWJjfc" -O augmented_wikiauto_max_cnt_2.pickle && rm -rf /tmp/cookies.txt
 wget -P /workspace/simpeval/marginranking/wikiauto/data/ --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=10oFocS84miPBOWXh5YrVYYRS8u2wkZvh' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=10oFocS84miPBOWXh5YrVYYRS8u2wkZvh" -O augmented_wikiauto_max_cnt_6.pickle && rm -rf /tmp/cookies.txt
+wget -P /workspace/simpeval/marginranking/wikiauto/data/ --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1woOcQgNXKGSOrOkOte23YFAOEC1zpyLj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1woOcQgNXKGSOrOkOte23YFAOEC1zpyLj" -O augmented_wikiauto_max_cnt_14.pickle && rm -rf /tmp/cookies.txt
+wget -P /workspace/simpeval/marginranking/wikiauto/data/ --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1ffEP26UINC-5sWXxcJ5TfCdCERl7Upzv' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ffEP26UINC-5sWXxcJ5TfCdCERl7Upzv" -O augmented_wikiauto_max_cnt_30.pickle && rm -rf /tmp/cookies.txt
 
 #finetuned checkpoints
-wget -P ../../data --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1Lytj61Rso_kR8jAU8YtT0-136hmSLCYM' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Lytj61Rso_kR8jAU8YtT0-136hmSLCYM" -O epoch=9.ckpt && rm -rf /tmp/cookies.txt
+wget -P /workspace/simpeval/marginranking/wikiauto/data/ --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1Lytj61Rso_kR8jAU8YtT0-136hmSLCYM' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Lytj61Rso_kR8jAU8YtT0-136hmSLCYM" -O epoch=9.ckpt && rm -rf /tmp/cookies.txt
+
+#random sampled data
+wget -P /workspace/simpeval/marginranking/wikiauto/data/ --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1QLPDxLQBZsY0-zWoRrH7HHL4BedvfcMM' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QLPDxLQBZsY0-zWoRrH7HHL4BedvfcMM" -O augmented_wikiauto_max_cnt_6_randomsamp_16_labeled_by_exp_1 && rm -rf /tmp/cookies.txt
 
 #exec prepare_data.py
 python ../../data/prepare_data.py
 
+
+https://drive.google.com/file/d/1QLPDxLQBZsY0-zWoRrH7HHL4BedvfcMM/view?usp=sharing
