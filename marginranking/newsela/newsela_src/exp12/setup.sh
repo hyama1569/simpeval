dec7a6735feaa38647aecba6e545057d9ca04141
Hikaru Yamanaka <sqrt1569@gmail.com>
2023-02-16 02:07:20 +0900

    add

---
+++ marginranking/newsela/newsela_src/exp12/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#requirements install
+pip install wandb
+wandb login
+#pip install git+https://github.com/facebookresearch/text-simplification-evaluation.git
+pip install transformers
+pip install pytorch-lightning==1.8.1
+pip install hydra-core --upgrade
